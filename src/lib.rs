96d6161b04348a6df770a62377471aa852586766
Sam Payson <scpayson@gmail.com>
2015-04-03 20:42:13 -0700

    Removed `feature` attributes for 1.0.0-beta.

--- src/lib.rs
+++ src/lib.rs
@@ -15,11 +15,6 @@
 // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-// Need feature(core) for tests, but the compiler complains that we're using the feature
-// unnecessarily when *not* building tests >_<.
-#![allow(unused_features)]
-#![feature(core, std_misc)]
-
 #[macro_export]
 macro_rules! __impl_float_type {
     ( $name:ident , $t:ty ) => {
